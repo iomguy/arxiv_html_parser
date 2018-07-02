7cf47b7512497e194d28ae557cc468418aa7ce7f
Alexander Kalyuzhnyuk <iom96@inbox.ru>
2018-07-02 12:58:29 +0300

    parts of code are divided into functions

--- parse_arxiv.py
+++ parse_arxiv.py
@@ -5,17 +5,18 @@ import pandas as pd
 import lxml.html as html
 import smtp
 import datetime
+from urllib.parse import urlparse
 
 
-def number_of_inclusions(substr_list, main_str):
+def number_of_inclusions(words_searched, text):
     """counts elements from substr_list that are included in main_str"""
 
-    included_key_words = [s for s in substr_list if s in main_str]
-    result = len(included_key_words)
-    return result, included_key_words
+    included_key_words = [s for s in words_searched if s in text]
+    number_of_included_key_words = len(included_key_words)
+    return number_of_included_key_words, included_key_words
 
 
-def form_data(page_content, key_words_list):
+def form_data(page_content, csv_columns, domain, key_words_list):
     """creates a dataframe with new articles that include key_words in abstracts"""
 
     print("-\nSuccessful connection to {}".format(url))
@@ -38,6 +39,7 @@ def form_data(page_content, key_words_list):
         for index, (link, meta_info) in enumerate(zip(links, info)):
 
             try:
+
                 meta_info_tags = meta_info.getchildren()
                 # first child HtmlElement with a "mathjax" class
                 meta_info_titles = next(
@@ -67,7 +69,7 @@ def form_data(page_content, key_words_list):
                         # if authors is not empty and "Authors" is the first element
                         authors = authors[1:] if "Authors" in authors[0] else authors
 
-                    except:
+                    except IndexError:
 
                         print("No-author article")
 
@@ -84,9 +86,9 @@ def form_data(page_content, key_words_list):
 
                     title_list.append("".join(titles))
                     authors_list.append(", ".join(authors))
-                    abstracts_list.append(domain_url + abs_link)
-                    pdf_list.append(domain_url + pdf_link)
-                    included_key_words_list.append(",".join(included_key_words))
+                    abstracts_list.append(domain + abs_link)
+                    pdf_list.append(domain + pdf_link)
+                    included_key_words_list.append(", ".join(included_key_words))
 
             except StopIteration:
 
@@ -102,16 +104,69 @@ def form_data(page_content, key_words_list):
         return submissions_data, index
 
 
+def form_data_to_csv(data, csv_columns, all_data_output_file, new_data_output_file):
+
+    if not os.path.isfile(all_data_output_file):
+        # if file does not exist write header
+        new_data = data
+        pd.DataFrame(columns=csv_columns). \
+            to_csv(all_data_output_file, sep=";", index=None)
+
+    else:
+        # else it exists so append without writing the header
+        source_data = pd.read_csv(all_data_output_file, sep=";")
+
+        # source and new data without duplicates
+        whole_unique_data = source_data.append(data, ignore_index=True) \
+            .drop_duplicates(subset=["Title", "Authors"], keep='first')
+
+        # only new data
+        new_data = source_data.append(whole_unique_data, ignore_index=True) \
+            .drop_duplicates(subset=["Title", "Authors"], keep=False)
+
+    number_of_res = data.shape[0]
+    print("-\n{} submissions with key words found".format(number_of_res))
+
+    number_of_new_res = new_data.shape[0]
+    print("-\n{} NEW submissions with key words found".format(number_of_new_res))
+
+    new_data.to_csv(all_data_output_file, sep=";", mode="a", header=False, index=None)
+    new_data.to_csv(new_data_output_file, sep=";", mode="w", index=None)
+    print("-\n.csv files are successfully finished!")
+
+    return number_of_new_res, new_data_output_file
+
+
+def send_mail(amount, attachment):
+    """sends file to the email and attaches a file if amount is a nonzero int"""
+
+    subject = "arXiv new articles " + datetime.datetime.today().strftime("%Y.%m.%d %H:%M:%S")
+    text = "{0} NEW submissions with key words found. {1} submissions in total" \
+        .format(amount, number_of_submissions_in_total)
+
+    if amount > 0:
+
+        file = [attachment]
+
+    else:
+
+        file = None
+
+    smtp.send_mail("theormechipmm@mail.ru", ["theormechipmm@mail.ru"], subject, text, files=file)
+    print("-\nemail is successfully sent!")
+
+
 if __name__ == "__main__":
 
     # TODO: используй arXiv API (https://github.com/zonca/python-parse-arxiv/blob/master/python_arXiv_parsing_example.py)
     # TODO: добавь нечувствительность к регистру
     key_words = ["ballistic heat transfer", "scalar lattice", "thermal processes", "harmonic crystal",
                  "kinetic temperature", "thermal"]
-    csv_columns = ["Title", "Authors", "Abstracts", "PDF", "Key_words"]
+    columns = ["Title", "Authors", "Abstracts", "PDF", "Key_words"]
 
-    domain_url = "https://arxiv.org"
     url = "https://arxiv.org/list/physics/new"
+    url_parsed = urlparse(url)
+    url_domain = "://".join([url_parsed.scheme, url_parsed.netloc])
 
     response = requests.get(url)
     page = html.fromstring(response.content)
@@ -120,55 +175,17 @@ if __name__ == "__main__":
 
     if response.status_code == 200:
         # successful connection
+        whole_data, number_of_submissions_in_total = form_data(csv_columns=columns,
+                                                               page_content=page,
+                                                               domain=url_domain,
+                                                               key_words_list=key_words)
 
-        data, number_of_submissions_in_total = form_data(page, key_words)
+        new_results_amount, new_results = form_data_to_csv(whole_data,
+                                                           csv_columns=columns,
+                                                           all_data_output_file="arXiv.csv",
+                                                           new_data_output_file="arXiv_new.csv")
 
-        all_data_output_file = "arXiv.csv"
-        new_data_output_file = "arXiv_new.csv"
-
-        if not os.path.isfile(all_data_output_file):
-            # if file does not exist write header
-            source_data = data
-            new_data = data
-            pd.DataFrame(columns=csv_columns).\
-                to_csv(all_data_output_file, sep=";", index=None)
-
-        else:
-            # else it exists so append without writing the header
-            source_data = pd.read_csv(all_data_output_file, sep=";")
-
-            # source and new data without duplicates
-            whole_unique_data = source_data.append(data, ignore_index=True)\
-                .drop_duplicates(subset=csv_columns, keep='first')
-
-            # only new data
-            new_data = source_data.append(whole_unique_data, ignore_index=True)\
-                .drop_duplicates(subset=csv_columns, keep=False)
-
-        number_of_res = data.shape[0]
-        print("-\n{} submissions with key words found".format(number_of_res))
-
-        number_of_new_res = new_data.shape[0]
-        print("-\n{} NEW submissions with key words found".format(number_of_new_res))
-
-        new_data.to_csv(all_data_output_file, sep=";", mode="a", header=False, index=None)
-        new_data.to_csv(new_data_output_file, sep=";", mode="w", index=None)
-        print("-\n.csv files are successfully finished!")
-
-        subject = "arXiv new articles " + datetime.datetime.today().strftime("%Y.%m.%d %H:%M:%S")
-        text = "{0} NEW submissions with key words found. {1} submissions in total" \
-            .format(number_of_new_res, number_of_submissions_in_total)
-
-        if number_of_new_res > 0:
-
-            file = [new_data_output_file]
-
-        else:
-
-            file = None
-
-        smtp.send_mail("theormechipmm@mail.ru", ["theormechipmm@mail.ru"], subject, text, files=file)
-        print("-\nemail is successfully sent!")
+        send_mail(new_results_amount, new_results)
 
     else:
         # unsuccessful connection
